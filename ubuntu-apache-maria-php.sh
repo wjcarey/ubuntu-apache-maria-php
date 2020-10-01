7c27e08f4d04a1c0a49284b0105e72371f6f9307
WJ Carey <wjcarey_personal@pop-os.localdomain>
2020-10-01 12:42:45 -0600

    removed apache and php config modify ability

--- ubuntu-apache-maria-php.sh
+++ ubuntu-apache-maria-php.sh
@@ -4,17 +4,6 @@
 echo "installing apache, mariadb, and php ..."
 apt update -qq && apt upgrade -qq && apt install -qq git unzip php php-cli php-fpm php-json php-pdo php-mysql php-zip php-gd php-mbstring php-curl php-xml php-pear php-bcmath php-tokenizer php-imagick mariadb-server
 
-#APACHE-PHP MODIFY CONFIG
-echo "adding rewrite rules to apache ..."
-a2enmod rewrite expires headers
-sed -i '0,/AllowOverride None/s//AllowOverride All/; 2,/AllowOverride None/s//AllowOverride All/; 0,/Require all denied/s//Require all granted/' /etc/apache2/apache2.conf
-echo "silencing apache server tokens from server ..."
-printf "\n#Remove Server Tokens\nServerTokens Prod\n#Remove server signature\nServerSignature Off" >> /etc/apache2/apache2.conf
-echo "updating php.ini for higher memory and upload size ..."
-sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 1G/g;s/post_max_size = 8M/post_max_size = 1G/g;s/memory_limit = 128M/memory_limit = 512M/g;s/max_execution_time = 30/max_execution_time = 300/g;s/max_input_time = 60/max_input_time = 300/g;' /etc/php/7.4/apache2/php.ini
-echo "restarting apache2 ..."
-systemctl restart apache2
-
 #DATABASE CONFIGURE
 echo "Would you like to configure the database now? [Y/n]"
 read CONFIRM_DATABASE_CONFIGURE
