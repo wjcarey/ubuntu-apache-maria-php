8bbf5e708c20b4c6e7e47d96e41cde5dcd0b90eb
WJ Carey <wjcarey_personal@pop-os.localdomain>
2020-10-12 16:36:04 -0600

    allowed argument to be passed into database config

--- README.md
+++ README.md
@@ -4,3 +4,13 @@
 ~~~
 sudo curl -o ubuntu-apache-maria-php.sh https://raw.githubusercontent.com/wjcarey/ubuntu-apache-maria-php/master/ubuntu-apache-maria-php.sh && sudo chmod 777 ubuntu-apache-maria-php.sh && sudo ./ubuntu-apache-maria-php.sh
 ~~~
+
+## Database Arguments can be passed into this command (currently you must use all three)
+1 = database name
+2 = sql username
+3 = sql password
+
+### Example script with defaule values added (default, admin, password)
+~~~
+sudo curl -o ubuntu-apache-maria-php.sh https://raw.githubusercontent.com/wjcarey/ubuntu-apache-maria-php/master/ubuntu-apache-maria-php.sh && sudo chmod 777 ubuntu-apache-maria-php.sh && sudo ./ubuntu-apache-maria-php.sh default admin password
+~~~
--- ubuntu-apache-maria-php.sh
+++ ubuntu-apache-maria-php.sh
@@ -5,29 +5,42 @@ echo "updating linux then installing apache, mariadb, and php ..."
 apt update -y && apt upgrade -y && apt install -y git unzip php php-cli php-fpm php-json php-pdo php-mysql php-zip php-gd php-mbstring php-curl php-xml php-pear php-bcmath php-tokenizer php-imagick mariadb-server
 echo "success: software install complete ..."
 
-#DATABASE CONFIGURE
-echo "would you like to configure the database now? [Y/n]"
-read CONFIRM_DATABASE_CONFIGURE
-if [ "$CONFIRM_DATABASE_CONFIGURE" != "${CONFIRM_DATABASE_CONFIGURE#[Yy]}" ] ;then
-    echo "starting database configuration ..."
-    echo "create your username ..."
-    read DATABASE_USER
-    echo "create your password for ${DATABASE_USER} ..."
-    read DATABASE_PASS
-    echo "create your database Name ..."
-    read DATBASE_NAME
-    echo "user: ${DATABASE_USER}, password: ${DATABASE_PASS}, database: ${DATBASE_NAME}, do you want to continue? [Y/n]"
-    read CONFIRM_DATABASE_INSTALL
-    if [ "$CONFIRM_DATABASE_INSTALL" != "${CONFIRM_DATABASE_INSTALL#[Yy]}" ] ;then
-        mysql -e "DROP USER 'root'@'localhost'; CREATE USER 'root'@'%' IDENTIFIED BY ''; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION; CREATE USER '${DATABASE_USER}'@'%' IDENTIFIED BY '${DATABASE_PASS}'; GRANT ALL PRIVILEGES ON *.* TO '${DATABASE_USER}'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-        mysql -e "CREATE DATABASE ${DATBASE_NAME}; FLUSH PRIVILEGES;"
-        echo "Success: database complete ..."
+#VARIABLES
+echo "gathering arguments from commandline ..."
+if [ -z "$1" ]
+    then
+        echo -e "\e[32mwould you like to configure the database now? [Y/n]"
+        read CONFIRM_DATABASE_CONFIGURE
+
+        #USER PROMPT CONFIGURE
+        if [ "$CONFIRM_DATABASE_CONFIGURE" != "${CONFIRM_DATABASE_CONFIGURE#[Yy]}" ] ;then
+            echo "starting database configuration ..."
+            echo -e "\e[32mcreate your database Name ..."
+            read DATBASE_NAME
+            echo -e "\e[32mcreate your username ..."
+            read DATABASE_USER
+            echo -e "\e[32mcreate your password for ${DATABASE_USER} ..."
+            read DATABASE_PASS
+            echo -e "\e[39mdatabase: \e[32m${DATABASE_NAME}\e[39m, user: \e[32m${DATABASE_USER}\e[39m, password: \e[32m${DATABASE_PASS}\e[39m, do you want to continue? [Y/n]"
+            read CONFIRM_DATABASE_INSTALL
+            if [ "$CONFIRM_DATABASE_INSTALL" != "${CONFIRM_DATABASE_INSTALL#[Yy]}" ] ;then
+                mysql -e "DROP USER 'root'@'localhost'; CREATE USER 'root'@'%' IDENTIFIED BY ''; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION; CREATE USER '${DATABASE_USER}'@'%' IDENTIFIED BY '${DATABASE_PASS}'; GRANT ALL PRIVILEGES ON *.* TO '${DATABASE_USER}'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+                mysql -e "CREATE DATABASE ${DATBASE_NAME}; FLUSH PRIVILEGES;"
+                echo "Success: database complete ..."
+            else
+                echo "Notice: database configuration exited by user ..."
+            fi
+        else
+            echo "notice: database configuration skipped ..."
+        fi
     else
-        echo "Notice: database configuration exited by user ..."
-    fi
-else
-    echo "notice: database configuration skipped ..."
+        #ARGUMENT CONFIGURE
+        mysql -e "DROP USER 'root'@'localhost'; CREATE USER 'root'@'%' IDENTIFIED BY ''; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION; CREATE USER '${3}'@'%' IDENTIFIED BY '${4}'; GRANT ALL PRIVILEGES ON *.* TO '${DATABASE_USER}'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+        mysql -e "CREATE DATABASE ${2}; FLUSH PRIVILEGES;"
+        echo "Success: database complete ..."
+
 fi
 
+#SELF DELETE AND EXIT
 rm -- "$0"
 exit
