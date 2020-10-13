9c2560406c024973549ab5a06f6389f7ab6783af
WJ Carey <wjcarey_personal@pop-os.localdomain>
2020-10-13 16:09:08 -0600

    better argument flow

--- ubuntu-apache-maria-php.sh
+++ ubuntu-apache-maria-php.sh
@@ -6,13 +6,10 @@ apt update -y && apt upgrade -y && apt install -y git unzip php php-cli php-fpm 
 echo "success: software install complete ..."
 
 #VARIABLES
-echo "gathering arguments from commandline ..."
 if [ -z "$1" ]
     then
         echo -e "\e[32mwould you like to configure the database now? [Y/n]"
         read CONFIRM_DATABASE_CONFIGURE
-
-        #USER PROMPT CONFIGURE
         if [ "$CONFIRM_DATABASE_CONFIGURE" != "${CONFIRM_DATABASE_CONFIGURE#[Yy]}" ] ;then
             echo "starting database configuration ..."
             echo -e "\e[32mcreate your database Name ..."
@@ -23,23 +20,26 @@ if [ -z "$1" ]
             read DATABASE_PASS
             echo -e "\e[39mdatabase: \e[32m${DATABASE_NAME}\e[39m, user: \e[32m${DATABASE_USER}\e[39m, password: \e[32m${DATABASE_PASS}\e[39m, do you want to continue? [Y/n]"
             read CONFIRM_DATABASE_INSTALL
-            if [ "$CONFIRM_DATABASE_INSTALL" != "${CONFIRM_DATABASE_INSTALL#[Yy]}" ] ;then
-                mysql -e "DROP USER 'root'@'localhost'; CREATE USER 'root'@'%' IDENTIFIED BY ''; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION; CREATE USER '${DATABASE_USER}'@'%' IDENTIFIED BY '${DATABASE_PASS}'; GRANT ALL PRIVILEGES ON *.* TO '${DATABASE_USER}'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-                mysql -e "CREATE DATABASE ${DATBASE_NAME}; FLUSH PRIVILEGES;"
-                echo "Success: database complete ..."
-            else
+            if [ "$CONFIRM_DATABASE_INSTALL" != "${CONFIRM_DATABASE_INSTALL#[Nn]}" ] ;then
                 echo "Notice: database configuration exited by user ..."
+                #SELF DELETE AND EXIT
+                rm -- "$0"
+                exit
             fi
         else
             echo "notice: database configuration skipped ..."
+            #SELF DELETE AND EXIT
+            rm -- "$0"
+            exit
         fi
     else
-        #ARGUMENT CONFIGURE
-        mysql -e "DROP USER 'root'@'localhost'; CREATE USER 'root'@'%' IDENTIFIED BY ''; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION; CREATE USER '${2}'@'%' IDENTIFIED BY '${3}'; GRANT ALL PRIVILEGES ON *.* TO '${2}'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-        mysql -e "CREATE DATABASE ${1}; FLUSH PRIVILEGES;"
-        echo "Success: database complete ..."
-
+    DATBASE_NAME=${1}
+    DATABASE_USER=${2}
+    DATABASE_PASS=${3}
 fi
+mysql -e "DROP USER 'root'@'localhost'; CREATE USER 'root'@'%' IDENTIFIED BY ''; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION; CREATE USER '${2}'@'%' IDENTIFIED BY '${3}'; GRANT ALL PRIVILEGES ON *.* TO '${2}'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+mysql -e "CREATE DATABASE ${1}; FLUSH PRIVILEGES;"
+echo "Success: database complete ..."
 
 #SELF DELETE AND EXIT
 rm -- "$0"
