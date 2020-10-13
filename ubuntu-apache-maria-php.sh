9b4aafce8a5124a2ff586c3c25eb8e9640a78cfa
WJ Carey <wjcarey_personal@pop-os.localdomain>
2020-10-13 16:21:38 -0600

    remove extra if else logic

--- ubuntu-apache-maria-php.sh
+++ ubuntu-apache-maria-php.sh
@@ -20,27 +20,18 @@ if [ -z "$1" ]
             read DATABASE_PASS
             echo -e "\e[39mdatabase: \e[32m${DATABASE_NAME}\e[39m, user: \e[32m${DATABASE_USER}\e[39m, password: \e[32m${DATABASE_PASS}\e[39m, do you want to continue? [Y/n]"
             read CONFIRM_DATABASE_INSTALL
-            if [ "$CONFIRM_DATABASE_INSTALL" != "${CONFIRM_DATABASE_INSTALL#[Nn]}" ] ;then
-                echo "Notice: database configuration exited by user ..."
-                #SELF DELETE AND EXIT
-                rm -- "$0"
-                exit
-            fi
-        else
-            echo "notice: database configuration skipped ..."
-            #SELF DELETE AND EXIT
-            rm -- "$0"
-            exit
         fi
     else
     DATBASE_NAME=${1}
     DATABASE_USER=${2}
     DATABASE_PASS=${3}
+    CONFIRM_DATABASE_INSTALL="y"
 fi
-mysql -e "DROP USER 'root'@'localhost'; CREATE USER 'root'@'%' IDENTIFIED BY ''; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION; CREATE USER '${2}'@'%' IDENTIFIED BY '${3}'; GRANT ALL PRIVILEGES ON *.* TO '${2}'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-mysql -e "CREATE DATABASE ${1}; FLUSH PRIVILEGES;"
-echo "Success: database complete ..."
-
+if [ "$CONFIRM_DATABASE_INSTALL" != "${CONFIRM_DATABASE_INSTALL#[Yy]}" ] ;then
+    mysql -e "DROP USER 'root'@'localhost'; CREATE USER 'root'@'%' IDENTIFIED BY ''; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION; CREATE USER '${2}'@'%' IDENTIFIED BY '${3}'; GRANT ALL PRIVILEGES ON *.* TO '${2}'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+    mysql -e "CREATE DATABASE ${1}; FLUSH PRIVILEGES;"
+    echo "Success: database complete ..."
+fi
 #SELF DELETE AND EXIT
 rm -- "$0"
 exit
